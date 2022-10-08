cc50f153bf0bb6974b25d9a7f473a1207deaa693
guime7000 <tguimezanes@gmail.com>
2022-10-08 23:15:18 +0200

    more versatile project map access using new function getArborescence.py

--- Src/getToken.py
+++ Src/getToken.py
@@ -5,6 +5,8 @@ import time
 from datetime import datetime
 import os
 
+import getArborescence
+
 """
     Retrieves a 1 hour validity Token after checking the current token isn't valid anymore
 
@@ -17,37 +19,43 @@ import os
 """
 
 # Loads VR email account and password File
-
 with open('/home/tom/Developpement/Git/MOwnR/Prod/Secret/email.config', 'r') as accountFile :
     accountParams = accountFile.readlines()
 
 # Loads Project Map JSON file :
-
-with open('/home/tom/Developpement/Git/MOwnR/Prod/Src/projectMap.JSON', 'r') as mapFile :
-    arborescence = json.load(mapFile)
+arborescence = getArborescence.getArborescence()
 
 # Sets Token log file path
-logging.basicConfig(filename=os.path.join(arborescence['root'],arborescence['storage']['data']['log'],'token.log'), encoding='utf-8', level=logging.INFO)
+logFileName = 'token.log'
+logFilePath = os.path.join(arborescence['root'],arborescence['storage']['data']['log'],logFileName)
+logging.basicConfig(filename=logFilePath, encoding='utf-8', level=logging.INFO)
 
 apiResponseError = True
+tokenFilePath = os.path.join(arborescence['root'],arborescence['storage']['data']['wnd'],'token.JSON')
 
 while apiResponseError:
 
     tokenTimestamp = int(time.time())
-
     loginHeaders = arborescence['api']['login']['headers']
-    loginDict = json.JSONEncoder().encode({"email": accountParams[0].replace("\n",""),"password": str(accountParams[1]),"raceId": arborescence['api']['raceId'],"legNum": arborescence['api']['leg_num']})
+    loginDict = json.JSONEncoder().encode({"email": accountParams[0].replace("\n",""),
+                                           "password": str(accountParams[1]),
+                                           "raceId": arborescence['api']['raceId'],
+                                           "legNum": arborescence['api']['leg_num']})
+
     urlString = arborescence['api']['url'] + arborescence['api']['login']['subUrl']
     response = requests.post(urlString, data = loginDict, headers = loginHeaders)
 
     if response.status_code == 200 :
-        with open(os.path.join(arborescence['root'],arborescence['storage']['data']['wnd'],'token.JSON'), 'w') as outfile :
+        with open(tokenFilePath, 'w') as outfile :
             json.dump({'exportTs': tokenTimestamp, 'token': response.headers['Token']},outfile,indent = 2)
 
-        logging.info(f"{datetime.utcfromtimestamp(tokenTimestamp).strftime('%Y-%m-%d %H:%M:%S')} : Token written to {os.path.join(arborescence['root'],arborescence['storage']['data']['wnd'],'token.JSON')}" )
+        logTimestamp = datetime.utcfromtimestamp(tokenTimestamp).strftime('%Y-%m-%d %H:%M:%S')
+        logging.info(f"{logTimestamp} : Token written to {tokenFilePath}")
+
         apiResponseError = False
+
     else :
-        logging.error(f"[ERR{datetime.utcfromtimestamp(tokenTimestamp).strftime('%Y-%m-%d %H:%M:%S')} : code {response.status_code}" )
+        logging.error(f"{logTimestamp} : code {response.status_code}" )
         time.sleep(30)
 
 
